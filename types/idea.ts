b55656f6cabc68fae80066b039c63f34ecaf8253
odiseo153 <odiseorincon@gmail.com>
2025-12-28 19:10:06 +0000

    poniendo columnas opcionales

--- types/idea.ts
+++ types/idea.ts
@@ -29,7 +29,7 @@ export interface Idea {
   db_structure?: string | null;
   redes_sociales: string[];
   images_prompt: string[];
-  created_at: string | null;
-  updated_at: string | null;
+  created_at?: string | null;
+  updated_at?: string | null;
 }
 
